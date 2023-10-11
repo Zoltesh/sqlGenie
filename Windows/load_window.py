f33c6a57bbdafe7d53eed345313166901f6d9a6f
zoltesh <braydude15@gmail.com>
2023-10-11 09:17:49 -0600

    Optimized UI by removing child frames.

--- Windows/load_window.py
+++ Windows/load_window.py
@@ -8,9 +8,26 @@ import customtkinter as ctk
 from tkinter import filedialog
 
 
-def load_file():
+def load_new_file():
+    # Check if changes made to currently loaded file
+    # If no changes, begin dialogue to load new file
+    # If changes made to currently loaded file, dialogue to save changes
+    # After save or no save, dialogue to open new file
     query_file = filedialog.askopenfile()
     print(query_file)
+
+
+def save_to_file():
+    # Select file to save to
+    # Make sure loaded file is same file being written to
+    pass
+
+
+def populate_fields():
+    # Insert data from loaded query into vars and description
+    # Selected var ON SELECT should display partial line where value resides, with the value being
+    # highlighted or otherwise stand out
+    pass
 
 
 class LoadWindow:
@@ -27,26 +44,31 @@ class LoadWindow:
         self.child_frame_2_1 = None
         self.child_frame_2_2 = None
         self.child_frame_2_3 = None
+        self.child_frame_2_4 = None
 
         # Frame 2 child 1 widgets
-        self.placeholder_label_2_1 = None
-        self.placeholder_combobox_2_1 = None
-        self.placeholder_textbox_2_1 = None
+        self.placeholder_label_2 = None
+        self.placeholder_combobox_2 = None
+        self.placeholder_textbox_2 = None
 
         # Frame 2 child 2 widgets
-        self.placeholder_textbox_2_2 = None
+        self.placeholder_textbox_2 = None
 
         # Frame 2 child 3 widgets
-        self.description_label_2_3 = None
-        self.description_textbox_2_3 = None
+        self.description_label_2 = None
+
+        # Frame 2 child 4 widgets
+        self.description_textbox_2 = None
 
         # Frame 3 children
         self.child_frame_3_1 = None
         self.child_frame_3_2 = None
 
         # Frame 3 widgets
-        self.save_btn_3_2 = None
-        self.cancel_btn_3_1 = None
+        self.query_output_textbox_3 = None
+        self.save_btn_3 = None
+        self.load_new_btn_3 = None
+        self.cancel_btn_3 = None
 
         self.filled_value_combobox = None
         self.button_font = None
@@ -77,38 +99,63 @@ class LoadWindow:
         self.app.minsize(800, 600)
         self.app.grid_columnconfigure(0, weight=1)
         self.app.grid_rowconfigure(0, weight=1)
-        self.app.grid_rowconfigure(1, weight=5)
-        self.app.grid_rowconfigure(2, weight=1)
+        self.app.grid_rowconfigure(1, weight=2)
+        self.app.grid_rowconfigure(2, weight=5)
 
         # Create frames for each column
         self.parent_frame_1 = ctk.CTkFrame(self.app)
         self.parent_frame_1.grid(row=0, column=0, sticky='nsew')
         self.parent_frame_1.columnconfigure(0, weight=1)
+        self.parent_frame_1.rowconfigure(0, weight=0)
 
         self.parent_frame_2 = ctk.CTkFrame(self.app)
         self.parent_frame_2.grid(row=1, column=0, sticky='nsew')
         self.parent_frame_2.columnconfigure(0, weight=0)
-        self.parent_frame_2.columnconfigure(1, weight=3)
+        self.parent_frame_2.columnconfigure(1, weight=0)
+        self.parent_frame_2.columnconfigure(2, weight=3)
+        self.parent_frame_2.rowconfigure(0, weight=0)
+        self.parent_frame_2.rowconfigure(1, weight=0)
 
         self.parent_frame_3 = ctk.CTkFrame(self.app)
         self.parent_frame_3.grid(row=2, column=0, sticky='nsew')
         self.parent_frame_3.columnconfigure(0, weight=1)
         self.parent_frame_3.columnconfigure(1, weight=1)
+        self.parent_frame_3.columnconfigure(2, weight=1)
+        self.parent_frame_3.rowconfigure(0, weight=3)
+        self.parent_frame_3.rowconfigure(1, weight=1)
 
         # Frame 1's Widgets
         self.window_title_1 = ctk.CTkLabel(self.parent_frame_1, text='Load Query', font=self.title_font)
         self.window_title_1.grid(row=0, column=0, sticky='nsew')
 
-        # Frame 2 child 1 widgets
+        # Frame 2's widgets
+        self.placeholder_label_2 = ctk.CTkLabel(self.parent_frame_2, text='Variables', font=self.label_font,
+                                                width=30)
+        self.placeholder_label_2.grid(row=0, column=0, padx=(10, 0), sticky='w')
+
+        self.placeholder_combobox_2 = ctk.CTkComboBox(self.parent_frame_2, values=['1', '2'])
+        self.placeholder_combobox_2.grid(row=0, column=1, sticky='nsew', padx=(10, 0))
+
+        self.placeholder_textbox_2 = ctk.CTkTextbox(self.parent_frame_2, font=self.text_font, height=30)
+        self.placeholder_textbox_2.grid(row=0, column=2, sticky='nsew', padx=10)
+
+        self.description_label_2 = ctk.CTkLabel(self.parent_frame_2, text='Description', font=self.label_font,
+                                                width=30)
+        self.description_label_2.grid(row=1, column=0, sticky='nw', padx=10)
+
+        self.description_textbox_2 = ctk.CTkTextbox(self.parent_frame_2, font=self.text_font)
+        self.description_textbox_2.grid(row=1, column=1, columnspan=2, sticky='nsew', pady=10, padx=10)
+
+        """# Frame 2 child 1 widgets
         self.child_frame_2_1 = ctk.CTkFrame(self.parent_frame_2)
         self.child_frame_2_1.grid(row=0, column=0, sticky='nsew')
 
         self.placeholder_label_2_1 = ctk.CTkLabel(self.child_frame_2_1, text='Variables', font=self.label_font,
                                                   height=30, width=30)
-        self.placeholder_label_2_1.grid(row=0, column=0, padx=20, sticky='w')
+        self.placeholder_label_2_1.grid(row=0, column=0, padx=(10, 0), sticky='nsew')
 
         self.placeholder_combobox_2_1 = ctk.CTkComboBox(self.child_frame_2_1, values=['1', '2'], height=30)
-        self.placeholder_combobox_2_1.grid(row=0, column=1, sticky='nsew')
+        self.placeholder_combobox_2_1.grid(row=0, column=1, sticky='nsew', padx=(10, 0))
 
         # Frame 2 child 2 widgets
         self.child_frame_2_2 = ctk.CTkFrame(self.parent_frame_2)
@@ -116,37 +163,40 @@ class LoadWindow:
         self.child_frame_2_2.columnconfigure(0, weight=1)
 
         self.placeholder_textbox_2_2 = ctk.CTkTextbox(self.child_frame_2_2, font=self.text_font, height=30)
-        self.placeholder_textbox_2_2.grid(row=0, column=0, sticky='ew')
+        self.placeholder_textbox_2_2.grid(row=0, column=0, sticky='nsew', padx=10)
 
         # Frame 2 child 3 widgets
         self.child_frame_2_3 = ctk.CTkFrame(self.parent_frame_2)
         self.child_frame_2_3.grid(row=1, column=0, sticky='nsew')
+        self.child_frame_2_3.columnconfigure(0, weight=0)
+        self.child_frame_2_3.columnconfigure(1, weight=1)
 
         self.description_label_2_3 = ctk.CTkLabel(self.child_frame_2_3, text='Description', font=self.label_font,
                                                   height=30, width=30)
-        self.description_label_2_3.grid(row=0, column=0, sticky='nw', padx=20)
+        self.description_label_2_3.grid(row=0, column=0, sticky='nsew', padx=10)
 
-        self.description_textbox_2_3 = ctk.CTkTextbox(self.child_frame_2_3, font=self.text_font)
-        self.description_textbox_2_3.grid(row=0, column=1, sticky='nsew', pady=10)
+        # Frame 2 child 4 widgets
+        self.child_frame_2_4 = ctk.CTkFrame(self.parent_frame_2)
+        self.child_frame_2_4.grid(row=1, column=1, sticky='nsew')
+        self.child_frame_2_4.columnconfigure(0, weight=1)
+        self.description_textbox_2_4 = ctk.CTkTextbox(self.child_frame_2_4, font=self.text_font)
+        self.description_textbox_2_4.grid(row=0, column=0, sticky='nsew', pady=10, padx=10)"""
 
-        # Frame 3 child 1 widgets
-        self.child_frame_3_1 = ctk.CTkFrame(self.parent_frame_3)
-        self.child_frame_3_1.grid(row=0, column=0, sticky='nsew')
-        self.child_frame_3_1.columnconfigure(0, weight=1)
-        self.child_frame_3_1.rowconfigure(0, weight=1)
+        # Frame 3 widgets
+        self.query_output_textbox_3 = ctk.CTkTextbox(self.parent_frame_3, font=self.text_font)
+        self.query_output_textbox_3.grid(row=0, column=0, columnspan=3, padx=10, sticky='nsew')
+        self.query_output_textbox_3.columnconfigure(0, weight=1)
+        self.query_output_textbox_3.rowconfigure(0, weight=1)
 
-        self.cancel_btn_3_1 = ctk.CTkButton(self.child_frame_3_1, text='Cancel', font=self.button_font, width=40,
-                                            command=self.app.destroy)
-        self.cancel_btn_3_1.grid(row=1, column=0, padx=50, sticky='w')
+        self.cancel_btn_3 = ctk.CTkButton(self.parent_frame_3, text='Cancel', font=self.button_font, width=40,
+                                          command=self.app.destroy)
+        self.cancel_btn_3.grid(row=1, column=0, padx=10, pady=10, sticky='sw')
 
-        # Frame 3 child 2 widgets
-        self.child_frame_3_2 = ctk.CTkFrame(self.parent_frame_3)
-        self.child_frame_3_2.grid(row=0, column=1, sticky='nsew')
-        self.child_frame_3_2.columnconfigure(0, weight=1)
+        self.load_new_btn_3 = ctk.CTkButton(self.parent_frame_3, text='Load New', font=self.button_font, width=40)
+        self.load_new_btn_3.grid(row=1, column=1, pady=10, sticky='s')
 
-        self.save_btn_3_2 = ctk.CTkButton(self.child_frame_3_2, text='Save', font=self.button_font, width=40)
-        self.save_btn_3_2.grid(row=1, column=0, padx=50, sticky='e')
-        self.child_frame_3_2.rowconfigure(0, weight=1)
+        self.save_btn_3 = ctk.CTkButton(self.parent_frame_3, text='Save', font=self.button_font, width=40)
+        self.save_btn_3.grid(row=1, column=2, padx=10, pady=10, sticky='se')
 
 
 def show_load_window(parent):
--- main.py
+++ main.py
@@ -24,16 +24,16 @@ class MainWindow:
         self.root.geometry('600x600')
 
         self.login_btn = ctk.CTkButton(self.root, text="Login", command=self.launch_login)
-        self.login_btn.pack(pady=20)
+        self.login_btn.grid(row=0, column=0, pady=20)
 
         self.load_btn = ctk.CTkButton(self.root, text="Load", command=self.launch_load)
-        self.load_btn.pack(pady=20)
+        self.load_btn.grid(row=1, column=0, pady=20)
 
         self.create_btn = ctk.CTkButton(self.root, text="Create")
-        self.create_btn.pack(pady=20)
+        self.create_btn.grid(row=2, column=0, pady=20)
 
         self.quit_btn = ctk.CTkButton(self.root, text="Quit", command=self.root.destroy)
-        self.quit_btn.pack(pady=20)
+        self.quit_btn.grid(row=3, column=0, pady=20)
 
     def launch_login(self):
         login_window.show_login_window(self.root)
